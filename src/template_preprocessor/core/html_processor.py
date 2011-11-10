ac0e77a29ac29e71eb284b37c674e81f11e172a7
twidi <s.angel@twidi.com>
2011-11-10 14:43:06 +0100

    Merge from upstream

--- src/template_preprocessor/core/html_processor.py
+++ src/template_preprocessor/core/html_processor.py
@@ -66,7 +66,7 @@ __HTML_ATTRIBUTES = {
     'select': ('name', 'value', 'size', ),
     'textarea': ('name', 'rows', 'cols', 'readonly', ),
     'link': ('type', 'rel', 'href', 'media', 'charset', ),
-    'meta': ('content', 'http-equiv', 'name', ),
+    'meta': ('content', 'http-equiv', 'name', 'charset', ),
     'script': ('type', 'src', 'language', 'charset', ),
     'style': ('type', 'media', ),
     'td': ('colspan', 'rowspan', ),
